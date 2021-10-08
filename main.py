16d630f40f90e1d581fa333b61b93b8a07789521
Zac R <zacharyr@ieee.org>
2021-10-08 19:40:36 -0400

    Added more libraries for obfuscation of bot token and some skeleton code for testing/brainstorming.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+# main.py 
+# Head function for the discord bot project
+
 import discord
 import asyncio
 import json
 import requests
+import os                                                                                                                                                                                                                                                                                                                    from dotenv import load_dotenv as load                                                                                                                                                                                                                                                                                          
+client = discord.Client()
 
-#client = discord.Client()
+load()
+
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+@client.event
+async def on_ready():
+   
+    # Verify that the guild the client is in is the desired guild according to the environment
+    for guild in client.guilds:
+        if guild.name == GUILD
+        break
+    print(
+        f'{client.user} here, ready to automate. In '
+        f'{guild.name}(id: {guild.id})'
+    )
+  
+client.run(BOT_TOKEN)
